57f991e0db3b1d950fdecc11f78c159d228fd38e
tnbnt <info@charr.dev>
2022-01-11 11:08:52 +0900

    Revert "Revert "use api.search_tweets instead of api.search and tweepy.Cursor""
    This reverts commit 05843c52e04a4d88f00052693bb3ed8823534a29.

--- hello_tweepy.py
+++ hello_tweepy.py
@@ -18,6 +18,20 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+# 検索する文章
+search_text = 'コロナ ワクチン'
+
+# 検索する
+search_results = api.search_tweets(q=search_text, count=100, lang='ja', result_type='mixed')
+
+# 検索結果を1件ずつ出力する
+for status in search_results:
+    print('---------------------------------------')
+    user = status.user
+    print('{}(@{}) フォロー数:{}, フォロワー数:{}\n\n{}\n\nいいね:{}, リツイート:{}'
+          .format(user.name, user.screen_name, user.friends_count, user.followers_count, status.text,
+                  status.favorite_count, status.retweet_count))
+
+#public_tweets = api.home_timeline()
+#for tweet in public_tweets:
+#    print(tweet.text)
